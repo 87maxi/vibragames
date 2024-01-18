6f039a643d32a767ed76a418ff4a0e3bb04400fa
maxi <maxi@localhost.localdomain>
2024-01-18 19:37:44 -0300

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+
+crear virtual venv
+python3.11 -m venv venv
+
+install pakage
+
+
+
+
+Commands
+
+flask db init
+
+flask db migrate -m "Initial migration."
---
+++ models/user.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    apellido = db.Column(db.String(128))
+    password = db.Column(db.String(8))
+    birthdate = db.Column(db.DateTime)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
